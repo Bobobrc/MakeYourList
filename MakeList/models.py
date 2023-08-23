4063b76f98233ed46f959a5784df960da71a9cb3
Bobobrc <bobo.bogdan122@gmail.com>
2023-08-23 16:50:29 +0300

    models for list and task

--- MakeList/models.py
+++ MakeList/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+# Create your models here.
+class List(models.Model):
+  list_name = models.CharField(max_length=50, unique=True, blank=False, null=False)
+  list_password = models.CharField(max_length=50, blank=False, null=False)
+  
+  def __str__(self):
+    return self.list_name
+  
+class Task(models.Model):
+  list = models.ForeignKey(List, on_delete = models.CASCADE)
+  task_name = models.CharField(max_length=200, blank=False, null=False)
+  
+  def __str__(self):
+    return self.task_name
