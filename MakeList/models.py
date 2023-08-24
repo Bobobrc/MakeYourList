86ba9acaa6ca476ab44fbcd41adfc6a85bc9085b
Bobobrc <bobo.bogdan122@gmail.com>
2023-08-24 20:02:20 +0300

    first page + create list + search list

--- MakeList/models.py
+++ MakeList/models.py
@@ -14,6 +14,8 @@ class List(models.Model):
 class Task(models.Model):
   list = models.ForeignKey(List, on_delete = models.CASCADE)
   task_name = models.CharField(max_length=200, blank=False, null=False)
+  important = models.BooleanField(default=False)
+  done = models.BooleanField(default=False)
   
   def __str__(self):
     return self.task_name
--- MakeList/views.py
+++ MakeList/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import Http404
+from .models import List
+from .forms import ListForm
 
 # Create your views here.
 def MainPage(request):
+  create_form = ListForm()
+  if 'create_list' in request.POST:
+    create_form = ListForm(request.POST)
+    if create_form.is_valid():
+      list_name = create_form.cleaned_data['list_name']
+      list_password = create_form.cleaned_date['list_password']
+      new_list = List(list_name = list_name, list_password = list_password)
+      new_list.save()
+      return redirect('ListPage', list_name = list_name)
+  elif 'search_list' in request.POST:
+    list_name = request.POST.get('list_name')
+    try:
+      existing_list = List.objects.get(list_name = list_name)
+      return redirect('ListPage', list_name = list_name)
+    except:
+      raise Http404("List does not exist!")
   return render(request, 'makelist/firstpage.html')
 
 def ListPage(request, list_name):
